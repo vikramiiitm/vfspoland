99684d8d4cc1733b154fad3b6e0acedb27523b22
vikramiiitm <vikramiiitmc@gmail.com>
2022-01-02 10:02:34 +0530

    done

--- m.py
+++ m.py
@@ -5,7 +5,9 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from twilio.rest import Client
-
+import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 def sendvineet():
     account_sid = 'ACdec1fc4ec94011e54ed96cbadceddef4' 
     auth_token = '611d6cc9e47471c342346247897b8de0' 
@@ -36,14 +38,15 @@ class vfs:
     def __init__(self):
         self.url = "https://visa.vfsglobal.com/ind/en/pol/login"
         Firefox_options = webdriver.FirefoxOptions()
+        Firefox_options.binary_location = os.environ.get("FIREFOX_BIN")
         Firefox_options.add_argument("--headless")
         Firefox_options.add_argument("--disable-dev-shm-usage")
         Firefox_options.add_argument("--no-sandbox")
-        self.driver = driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox(executable_path=os.environ.get("GECKODRIVER_PATH"),options=Firefox_options)
     
     def sendvineet():
         account_sid = 'ACdec1fc4ec94011e54ed96cbadceddef4' 
-        auth_token = '611d6cc9e47471c342346247897b8de0' 
+        auth_token = '[AuthToken]' 
         client = Client(account_sid, auth_token) 
 
         message = client.messages.create(  
@@ -69,7 +72,9 @@ class vfs:
     
     def login(self):
         self.driver.get(self.url)
-        time.sleep(5)
+        print("Login Page Opened")
+        options = Options()
+        options.add_argument("--headless")
         
         username = "vikramiiitmc@gmail.com"
         password = "Lhm@57284"
@@ -81,6 +86,17 @@ class vfs:
         login = True
         
         while login:
+            # try:
+            #     login_css = "/html/body/app-root/div/app-login/section/div/div/mat-card/form/button"
+            #     login_element = self.driver.find_element(By.XPATH,login_css).is_displayed()
+            #     print(login_element)
+            #     if not login_element:
+            #         print("Login Success")
+            #         break
+            #     self.driver.find_element(By.XPATH,login_css).click()
+            #     print("clicked login")
+            # except:
+            #     pass
             try:
                 if self.driver.current_url == "https://visa.vfsglobal.com/ind/en/pol/dashboard":
                     print("Login Success")
@@ -91,8 +107,7 @@ class vfs:
                     self.driver.find_element(By.XPATH,login_css).click()
                     print("clicked login")
             except:
-                pass          
-            time.sleep(2)    
+                pass                
         # Click New Booking
         time.sleep(3)
         self.driver.find_element(By.XPATH,'/html/body/app-root/div/app-dashboard/section/div/div[2]/button').click()
@@ -103,6 +118,7 @@ class vfs:
         # applicatiopn
         
         self.driver.find_element(By.XPATH,'//*[@id="mat-select-value-1"]').click()
+        time.sleep(3)
         self.driver.find_element(By.XPATH,'/html/body/div[5]/div[2]/div/div/div/mat-option[9]/span').click()
 
         time.sleep(3)
@@ -126,16 +142,12 @@ class vfs:
         except:
             pass
         print(status)
-        if status != "No appointment slots are currently available":
+        if status == "No appointment slots are currently available":
             # sendvikram()
             # sendvineet()
-            print("\nstatus : ",status," Msg sent")
+            print("status : ",status)
         
-        return status
-    
-    def close(self):
         self.driver.close()
-            
 VFS = vfs()
 VFS.login()
 # vineet
---
+++ script.py
@@ -0,0 +1,7 @@
+import vfs
+import time
+while True:
+    VFS = vfs.vfs()
+    VFS.login()
+    # VFS.close()
+    # time.sleep(120)
